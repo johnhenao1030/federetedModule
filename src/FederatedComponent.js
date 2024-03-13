96e0279d7e8dcec82f006668111b440d3256035b
John Angel Henao <jhon.henao@omni.pro>
2024-03-12 19:22:07 -0500

    prueba prop AEM

--- src/FederatedComponent.js
+++ src/FederatedComponent.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-const FederatedComponent = () => <div>Este es nuestro primer microfrontend</div>;
+// Asegúrate de que el componente acepte props
+const FederatedComponent = (props) => {
+    // Utiliza JSON.stringify para convertir el objeto EVENTO en una cadena de texto formateada
+    // Asegúrate de que props.EVENTO exista antes de intentar mostrarlo
+    const eventoString = props.EVENTO ? JSON.stringify(props.EVENTO, null, 2) : "No hay evento disponible";
+
+    return (
+        <div>
+            Este es nuestro primer microfrontend / prueba 1
+            {/* Muestra el JSON formateado dentro de un <pre> para mejor legibilidad */}
+            <pre>{eventoString}</pre>
+        </div>
+    );
+};
 
 export default FederatedComponent;
