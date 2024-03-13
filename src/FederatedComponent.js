a2ebee44b56b0e0c421b13e16bdea38fc96ca70f
John Angel Henao <jhon.henao@omni.pro>
2024-03-12 19:43:02 -0500

    reversar cambios

--- src/FederatedComponent.js
+++ src/FederatedComponent.js
@@ -1,18 +1,5 @@
 import React from "react";
 
-// Asegúrate de que el componente acepte props
-const FederatedComponent = (props) => {
-    // Utiliza JSON.stringify para convertir el objeto EVENTO en una cadena de texto formateada
-    // Asegúrate de que props.EVENTO exista antes de intentar mostrarlo
-    const eventoString = props.EVENTO ? JSON.stringify(props.EVENTO, null, 2) : "No hay evento disponible";
-
-    return (
-        <div>
-            Este es nuestro primer microfrontend / prueba 1
-            {/* Muestra el JSON formateado dentro de un <pre> para mejor legibilidad */}
-            <pre>{eventoString}</pre>
-        </div>
-    );
-};
+const FederatedComponent = () => <div>Este es nuestro primer microfrontend</div>;
 
 export default FederatedComponent;
